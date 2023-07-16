85b27dc0f61beb8a23418bfef5b6dd9878544de6
WilsonZhang746 <46818760+WilsonZhang746@users.noreply.github.com>
2023-07-16 15:52:08 +0200

    Add files via upload

--- R-Statistics.R
+++ R-Statistics.R
@@ -1916,5 +1916,62 @@ polyserial(mtcars$vs, mtcars$mpg)
 
 
 
+### Lecture 25. Mann Whitney U Test
+
+#example 1
+library(MASS)
+
+with(UScrime, by(Prob, So, median))
+
+
+wilcox.test(Prob ~ So, data=UScrime)
+
+
+#example 2
+
+# Creating a small dataset
+# Creating a vector of red bulb and orange prices
+red_bulb <- c(38.9, 61.2, 73.3, 21.8, 63.4, 64.6, 48.4, 48.8)
+orange_bulb <- c(47.8, 60, 63.4, 76, 89.4, 67.3, 61.3, 62.4)
+
+# Passing them in the columns
+BULB_PRICE = c(red_bulb, orange_bulb)
+BULB_TYPE = rep(c("red", "orange"), each = 8)
+
+# Now creating a dataframe
+DATASET <- data.frame(BULB_TYPE, BULB_PRICE, stringsAsFactors = TRUE)
+
+# printing the dataframe
+DATASET
+
+# installing libraries to view summaries and
+# boxplot of both orange and red color bulbs
+install.packages("dplyr")
+install.packages("ggpubr")
+
+# Summary of the data
+
+# loading the package
+library(dplyr)
+group_by(DATASET,BULB_TYPE) %>%
+  summarise(
+    count = n(),
+    median = median(BULB_PRICE, na.rm = TRUE),
+    IQR = IQR(BULB_PRICE, na.rm = TRUE))
+
+# loading package for boxplot
+library("ggpubr")
+ggboxplot(DATASET, x = "BULB_TYPE", y = "BULB_PRICE",
+          color = "BULB_TYPE", palette = c("#FFA500", "#FF0000"),
+          ylab = "BULB_PRICES", xlab = "BULB_TYPES")
+
+res <- wilcox.test(BULB_PRICE~ BULB_TYPE,
+                   data = DATASET,
+                   exact = FALSE)
+res
+
+
+
+
 
 
